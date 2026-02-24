b8b330fc333d4855c224ce3dc25a15dc3b3dc1d5
diksha238 <dikshanagpal2005@gmail.com>
2026-02-24 23:20:59 +0530

    Added Bank class to handle multiple accounts

--- src/Bank.java
+++ src/Bank.java
@@ -1,2 +1,19 @@
+import java.util.HashMap;
+
 public class Bank {
+    private HashMap<Integer,Account> accounts;
+    public Bank(){
+        accounts=new HashMap<>();
+    }
+    public void createAccount(int accNo,String name,int pin){
+        if(accounts.containsKey(accNo)) System.out.println("Account already exists");
+        else{
+            Account acc = new Account(accNo,name,pin);
+            accounts.put(accNo,acc);
+        }
+    }
+
+    public Account getAccount(int accNo) {
+        return accounts.get(accNo);
+    }
 }
